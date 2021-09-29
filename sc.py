f1c679b55f922f5396d51dea440982f4d00344fd
gusta <gustavo_o_@hotmail.com>
2021-09-29 15:59:59 -0400

    Simples exemplo de transmitir uma mensagem via socket com python3.
    Case 2 - Implementar para o servidor sempre ficar escultando, mesmo depois do cliente ter fechado a conexão

---
+++ sc.py
@@ -0,0 +1,26 @@
+import socket
+
+
+def conectar():
+    # Buscamo o Socket e passado a informação que vamos utlizar o protocolo TCP/IP no modo de Stream
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Realiza a conexão com o servidor que esta escultando.
+    cliente.connect(("127.0.0.1", 8989))
+
+    # Chamamos um input para informar uma mensagem para ser transmitida.
+    mensagem = input("Mensagem para ser transmitira para o servidor: ")
+
+    # Enviamos a mensagem informada via conexão estabelecida e codificando ela para ser transmitida.
+    cliente.send(mensagem.encode())
+
+    print("Mensagem enviada!")
+
+    # Fechando a conexão
+
+    cliente.close()
+
+
+conectar()
+
+
--- sv.py
+++ sv.py
@@ -1,5 +1,35 @@
 import socket
 
+
+import subprocess
+
+# Buscamo o Socket e passado a informação que vamos utlizar o protocolo TCP/IP no modo de Stream
 ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ss.bind((socket.gethostname(), 8989))
-ss.listen(5)
+
+# Essa é show... quando damos um quit no script ele para de escultar a conexao. Isso ajuda quando tem algum crash e iniciamos a aplicação novamente.
+ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Passammos as informações de porta e endereço que vamos ficar escultando.
+ss.bind(("localhost", 8989))
+
+# Vamos escultar 10 conexões
+ss.listen(10)
+
+print("Aguardando conexão!!!")
+
+# Aceitamos as conexões e passamos as informações recebidas do cliente
+conexao, cliente = ss.accept()
+
+
+with conexao:
+    print("Conectado com: ", cliente[0])
+    while True:
+        # Recebendo do dados passados com a conexão estabelecida, temos que informar o tamanho do byffer a ser transpotado, no caso 1024 bytes
+        recebe = conexao.recv(1024)
+        if not recebe:
+            break
+        # A mensagem transmitida codificada e usamos o decode para poder abrir ela.
+        print(recebe.decode())
+
+
+
